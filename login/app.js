aed0b870ab176764f1aa4052817fac9545cea5f1
Aljames21 <jrgaralde123@gmail.com>
2026-09-21 23:06:08 +0800

    fix: change title color to green upon successful login

--- login/app.js
+++ login/app.js
@@ -32,6 +32,12 @@ loginForm.addEventListener("submit", async (e) => {
 
     localStorage.setItem("token", data.token);
     document.getElementById("title").textContent = "Loading...";
+    loginForm.addEventListener(
+      "click", function () {
+        document.getElementById("title").style.color = "green";
+      }
+    ); //Add color change to the title when login is successful
+
     message.textContent = "Login Successful"; 
     setTimeout(() => {
     window.location.href = "../home/home.html";
