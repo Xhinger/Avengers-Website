7a146b7639f517a5a012824a6b20a10457c15b37
Aljames21 <jrgaralde123@gmail.com>
2026-09-21 22:46:50 +0800

    fix: redirect to home page after successful login with a delay

--- login/app.js
+++ login/app.js
@@ -31,7 +31,10 @@ loginForm.addEventListener("submit", async (e) => {
     }
 
     localStorage.setItem("token", data.token);
+    message.textContent = "Login Successful"; 
+    setTimeout(() => {
     window.location.href = "../home/home.html";
+    }, 1000);
   } catch (error) {
     message.textContent = "Cannot connect to the server.";
   }
